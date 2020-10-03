f175e7e326ede572c8ed39b4a052af0bc086765f
keltonjohnson004 <52982698+keltonjohnson004@users.noreply.github.com>
2020-10-03 12:52:40 -0600

    First Commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+
+    public float speed;
+
+    public Transform target;
+    public int targetIndex = 0;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+
+        direction = (target.position - transform.position).normalized;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= 0.1f)
+        {
+            GetNextTarget();
+        }
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        
+    }
+    
+    private void GetNextTarget()
+    {
+        if (targetIndex >= Waypoints.waypoints.Length - 1)
+        {
+            PathEnded();
+            return;
+        }
+
+        targetIndex++;
+        target = Waypoints.waypoints[targetIndex];
+
+    }
+
+    private void PathEnded()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    public static Transform[] waypoints;
+
+    private void Awake()
+    {
+        print(waypoints.Length);
+        waypoints = new Transform[waypoints.Length];
+        for(int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+
+}
